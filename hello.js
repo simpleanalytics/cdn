f396c8b910b87e6a97ad960a2184f0cfb54af728
Adriaan <adriaanvanrossum@users.noreply.github.com>
2019-02-16 20:19:14 +0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,30 +1,39 @@
+/* eslint-env browser */
+
 // We use https://jscompress.com to compress our script so you can test
 // if this script is indeed the same script as the minified version we
 // published on https://cdn.simpleanalytics.io/hello.js
 
-(function(window,d){
+(function(window, hostname, path, cdn) {
+  if (!window) return;
+
+  // Set urls outside try block because they are needed in the catch block
+  var protocol = 'https://';
+  var apiUrl = protocol + hostname + path;
+  var cdnUrl = cdn ? protocol + cdn : (apiUrl + '.js');
+  var con = window.console;
+
   try {
-    if (!window) return;
     var nav = window.navigator;
     var loc = window.location;
     var doc = window.document;
     var userAgent = nav.userAgent;
+    var dis = window.dispatchEvent;
     var lastSendUrl;
+    var notSending = 'Not sending requests ';
 
-    var script = doc.querySelector('script[src="https://cdn.simpleanalytics.io/hello.js"]')
+    var script = doc.querySelector('script[src="' + cdnUrl + '"]');
     var mode = script ? script.getAttribute('data-mode') : null;
-    
+
     // A simple log function so the user knows why a request is not being send
     var warn = function(message) {
-      if (console && console.warn) console.warn('Simple Analytics: ' + message);
+      if (con && con.warn) con.warn('Simple Analytics: ' + message);
     }
 
     // We do advanced bot detection in our API, but this line filters already most bots
-    if (userAgent.search(/(bot|spider|crawl)/ig) > -1) return warn('Not sending requests because user agent is a robot');
+    if (userAgent.search(/(bot|spider|crawl)/ig) > -1) return warn(notSending + 'because user agent is a robot');
 
-    var post = function(options) {
-      var isPushState = options && options.isPushState
-
+    var post = function(isPushState) {
       // Obfuscate personal data in URL by dropping the search and hash
       var url = loc.protocol + '//' + loc.hostname + loc.pathname;
 
@@ -36,26 +45,32 @@
       lastSendUrl = url;
 
       // Skip prerender requests
-      if ('visibilityState' in doc && doc.visibilityState === 'prerender') return warn('Not sending requests when prerender');
+      if ('visibilityState' in doc && doc.visibilityState === 'prerender') return warn(notSending + 'when prerender');
 
       // Don't track when Do Not Track is set to true
-      if ('doNotTrack' in nav && nav.doNotTrack === '1') return warn('Not sending requests when doNotTrack is enabled');
+      if ('doNotTrack' in nav && nav.doNotTrack === '1') return warn(notSending + 'when doNotTrack is enabled');
 
-      // Don't track when Do Not Track is set to true
-      if (loc.hostname === 'localhost') return warn('Not sending requests from localhost');
-  
+      // Don't track when localhost
+      if (loc.hostname === 'localhost' || loc.protocol === 'file:') return warn(notSending + 'from localhost');
+
       // From the search we grab the utm_source and ref and save only that
       var refMatches = loc.search.match(/[?&](utm_source|source|ref)=([^?&]+)/gi);
       var refs = refMatches ? refMatches.map(function(m) { return m.split('=')[1] }) : [];
 
-      var data = { source: 'js', url: url };
+      var data = { url: url };
       if (userAgent) data.ua = userAgent;
       if (refs && refs[0]) data.urlReferrer = refs[0];
       if (doc.referrer && !isPushState) data.referrer = doc.referrer;
       if (window.innerWidth) data.width = window.innerWidth;
 
+      try {
+        data.timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+      } catch (error) {
+        // nothing
+      }
+
       var request = new XMLHttpRequest();
-      request.open('POST', d + '/post', true);
+      request.open('POST', apiUrl, true);
 
       // We use content type text/plain here because we don't want to send an
       // pre-flight OPTIONS request
@@ -63,35 +78,37 @@
       request.send(JSON.stringify(data));
     }
 
-    // Thanks to https://gist.github.com/rudiedirkx/fd568b08d7bffd6bd372
     var his = window.history;
-    if (his && his.pushState && Event && window.dispatchEvent) {
+    var hisPushState = his ? his.pushState : null;
+    if (hisPushState && Event && dis) {
       var stateListener = function(type) {
         var orig = his[type];
         return function() {
           var rv = orig.apply(this, arguments);
           var event = new Event(type);
           event.arguments = arguments;
-          window.dispatchEvent(event);
+          dis(event);
           return rv;
         };
       };
-      his.pushState = stateListener('pushState');
+      hisPushState = stateListener('pushState');
       window.addEventListener('pushState', function() {
-        post({ isPushState: true });
+        post(true);
       });
     }
-    
+
     // When in hash mode, we record a pageview based on the onhashchange function
     if (mode === 'hash' && 'onhashchange' in window) {
-      window.onhashchange = post
+      window.onhashchange = function() {
+        post(true);
+      }
     }
 
     post();
   } catch (e) {
-    if (console && console.error) console.error(e);
-    var url = d + '/hello.gif';
+    if (con && con.error) con.error(e);
+    var url = apiUrl + '.gif';
     if (e && e.message) url = url + '?error=' + encodeURIComponent(e.message);
     new Image().src = url;
   }
-})(window, 'https://api.simpleanalytics.io');
+})(window, 'api.simpleanalytics.io', '/post', 'cdn.simpleanalytics.io/hello.js');
